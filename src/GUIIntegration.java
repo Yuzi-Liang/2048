6cf1c4b93b4a5be10d147d57d2d13a8638e271ec
Jiafan Lin <42848844+jeffson66@users.noreply.github.com>
2022-11-16 09:58:13 -0800

    initialize

---
+++ src/GUIIntegration.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GUIIntegration extends JFrame {
+    int[][] gameboard;
+    int size;
+    public GUIIntegration(int[][] gameboard){
+        this.gameboard = gameboard;
+        int size = gameboard.length;
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(size,size));
+        panel.
+    }
+
+
+}
